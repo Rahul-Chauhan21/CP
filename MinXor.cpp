808b2f2cfb6f7dda009d8e3a702dc6755ff5407d
Rahul Chauhan <crahul1721@gmail.com>
2019-04-25 00:01:34 +0530

    add

---
+++ MinXor.cpp
@@ -0,0 +1,38 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function to find minimum XOR pair
+int minXOR(int arr[], int n)
+{
+    // Sort given array
+    sort(arr, arr + n);
+
+    int minXor = INT_MAX;
+    int val = 0;
+
+    // calculate min xor of consecutive pairs
+    for (int i = 0; i < n - 1; i++) {
+        val = arr[i] ^ arr[i + 1];
+        minXor = min(minXor, val);
+    }
+
+    return minXor;
+}
+
+// Driver program
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+    cin>>arr[i];
+    }
+    //int n = sizeof(arr) / sizeof(arr[0]);
+    cout << minXOR(arr, n) << endl;
+
+    return 0;
+}
+
